f99080fe86c0ce6d8af6bf3796aee477a2a5ebad
Michael Farwell <mike@lie-nielsen.com>
2022-04-06 10:59:14 -0400

    Complete primitives module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 mod hello;
+mod primitives;
 
 fn main() {
+    println!("-Hello World-");
     hello::hello_world();
+    println!("\n-Formatted Print-");
     hello::formatted_print();
+    println!("\n-Debug-");
     hello::debug();
+    println!("\n-Display-");
     hello::display();
+    println!("\n-Testcase-");
     hello::testcase();
+    println!("\n-Formatting-");
     hello::formatting();
+
+    println!("\n-Primitives-");
+    primitives::primitives();
+    println!("\n-Literals & Operators-");
+    primitives::literals_and_operators();
+    println!("\n-Tuples-");
+    primitives::tuples();
+    println!("\n-Arrays & Slices-");
+    primitives::arrays_and_slices();
 }
---
+++ src/primitives.rs
@@ -0,0 +1,116 @@
+use std::fmt::{Display, Formatter};
+use std::mem;
+
+pub fn primitives() {
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0; // Regular annotation
+    let an_integer = 5i32; // Suffix annotation
+
+    let default_float = 3.0;
+    let default_integer = 7;
+
+    let mut inferred_type = 12;
+    inferred_type = 4294967296i64;
+
+    let mut mutable = 12;
+    mutable = 21;
+
+    // mutable = true; // Does not work
+    let mutable = true; // Works
+}
+
+pub fn literals_and_operators() {
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    println!("1 - 2 = {}", 1i32 - 2); // Using u32 would cause overflow
+
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    println!("On million is written as {}", 1_000_000);
+}
+
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (integer, boolean) = pair;
+    (boolean, integer)
+}
+
+fn transpose(matrix: &Matrix) -> Matrix {
+    Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
+}
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+impl Display for Matrix {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "( {}, {} )\n( {}, {} )", self.0, self.1, self.2, self.3)
+    }
+}
+
+pub fn tuples() {
+    let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                      -1i8, -2i16, -3i32, -4i64,
+                      0.1f32, 0.2f64, 'a', true);
+
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.1);
+
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    // println!("too long tuple: {:?}", too_long_tuple); // Tuple needs to have length less than 13
+
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    println!("one element tuple: {:?}", (5u32, ));
+    println!("just an integer: {:?}", (5u32));
+
+    let tuple = (1, "hello", 4.5, true);
+    let (a, b, c, d) = tuple;
+
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{}", matrix);
+    println!("{}", transpose(&matrix));
+    println!("Debug: {:?}", matrix);
+}
+
+fn analyze_slice(slice: &[i32]) {
+    println!("first element of the slice: {}", slice[0]);
+    println!("the slice has {} elements", slice.len());
+}
+
+pub fn arrays_and_slices() {
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+    let ys: [i32; 500] = [0; 500];
+
+    println!("first element of the array: {}", xs[0]);
+    println!("second element of the array: {}", xs[1]);
+
+    println!("number of elements in array: {}", xs.len());
+
+    println!("array occupies {} bytes", mem::size_of_val(&xs));
+
+    println!("borrow the whole array as a slice");
+    analyze_slice(&xs);
+
+    println!("borrow a section of the array as a slice");
+    analyze_slice(&ys[1..4]);
+
+    // println!("{}", xs[5]); // Won't compile.
+}
